81637f0a1ff6fc18c0c01d07d4e9c9e20cad4911
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-07-17 08:31:04 -0400

    Added basic card layout to body of albums app

--- albums/index.android.js
+++ albums/index.android.js
@@ -1,11 +1,17 @@
-// Import a library to help create a component
+// Import libraries to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+
+// Import child components from local directory
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-  <Header headerText={'Albums!'} />
+  <View>
+    <Header headerText={'Albums!'} />
+    <AlbumList />
+  </View>
 );
 
 // Render the component to the device
