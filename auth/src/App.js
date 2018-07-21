abbbcb443dc524086711f971be150c073ed52ccc
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-07-21 02:34:40 -0400

    Added auth app UI components and firebase client setup

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    const firebase = require('firebase'); // eslint-disable-line global-require
+    require('firebase/auth'); // eslint-disable-line global-require
+
+    firebase.initializeApp({
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      projectId: '',
+      storageBucket: '',
+      messagingSenderId: ''
+    });
+  }
+
   render() {
     return (
       <View>
-        <Header headerText="Authentication" />
-        <Text>An App</Text>
+        <Header headerText='Authentication' />
+        <LoginForm />
       </View>
     );
   }
