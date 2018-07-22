0e8eee4fc1c58ac736df48ae147268b0b0871d4f
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-07-22 14:08:13 -0400

    Added tech stack project

---
+++ tech_stack/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
+import reducers from './reducers';
+
+const App = () => (
+    <Provider store={createStore(reducers)}>
+      <View style={[]}>
+        <Header headerText='Tech Stack' />
+        <LibraryList />
+      </View>
+    </Provider>
+  );
+
+export default App;
